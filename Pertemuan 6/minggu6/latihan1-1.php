cb8ef006ed49c65568b18e3d47e0fc2fc6424af6
Fathan Rizki H <fathanrizki001@gmail.com>
2019-10-07 11:29:13 +0700

    Latihan 1.1-1.6

---
+++ Pertemuan 6/minggu6/latihan1-1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Contoh Skrip PHP</title>
+  </head>
+  <body>
+    <?php
+      $nama = "Kim Taehyung";
+      echo "$nama berkata \"save me save me\" ";
+     ?>
+
+</html>
---
+++ Pertemuan 6/minggu6/latihan2.php
@@ -0,0 +1,16 @@
+<?php
+  //Kalkulator Latihan 2
+  $a = 15;
+  $b = 5;
+
+  echo "Bilangan 1 = $a"; echo "<br>";
+  echo "Bilangan 2 = $b"; echo "<br>";
+  echo "Berikut hasil perhitungan dari setiap operator"; echo "<br>";
+
+    echo "<br/> PENJUMLAHAN <br/>";
+    $c = $a + $b;
+    echo "operator = +"; echo "<br>";
+    echo "hasilnya : $a + $b = $c";
+    echo "<br/>";
+    echo "<br/> ";
+ ?>
